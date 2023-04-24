5c4afc45eb75d3609bedcde3da428ee0cfed1bd9
MathewAkhil <90526563+MathewAkhil@users.noreply.github.com>
2023-04-24 15:28:40 -0500

    fixed up login and created signup

---
+++ process-signup.php
@@ -0,0 +1,34 @@
+<?php
+include "db_conn.php";
+
+// Get the form data
+$fnameR = $_POST['fnameRegister'];
+$lnameR = $_POST['lnameRegister'];
+$emailR = $_POST['emailRegister'];
+$passwordR = $_POST['passwordRegister'];
+$DOBR = $_POST['dateRegister'];
+$user_typeR = $_POST['typeRegister'];
+
+// Insert the new item into the database
+$sql = "INSERT INTO User (User_Email, User_Password, User_FName, User_LName, User_DOB, User_Type) VALUES ('$emailR', '$passwordR', '$fnameR', '$lnameR', '$DOBR', '$user_typeR')";
+$result = mysqli_query($conn, $sql);
+
+if ($result) {
+    echo "Account Successfully Created!";
+    // header("Location: index.php");
+    // exit();
+} else {
+    echo "Error creating account: " . mysqli_error($conn);
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <!-- <button><a href="index.php">Back to Login</a></button> -->
+    <form method="post" action="index.php">
+        <button type="submit">Go to Login</button>
+    </form>
+</head>
+</html>
+
---
+++ signup.php
@@ -0,0 +1,56 @@
+<?php
+
+session_start();
+include "db_conn.php";
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Signup</title>
+    <meta charset="UTF-8">
+</head>
+<body>
+    <h1>Signup</h1>
+
+    <form action="process-signup.php" method="post">
+        <div>
+            <label for="fnameRegister">First Name</label>
+            <input type="text" id="fnameRegister" name="fnameRegister" required>
+        </div>
+
+        <div>
+            <label for="lnameRegister">Last Name</label>
+            <input type="text" id="lnameRegister" name="lnameRegister" required>
+        </div>
+
+        <div>
+            <label for="emailRegister">Email</label>
+            <input type="email" id="emailRegister" name="emailRegister" required>
+        </div>
+
+        <div>
+            <label for="passwordRegister">Password</label>
+            <input type="password" id="passwordRegister" name="passwordRegister" required>
+        </div>
+
+        <div>
+            <label for="dateRegister">DOB</label>
+            <input type="date" id="dateRegister" name="dateRegister" required>
+        </div>
+
+        <div>
+            <label for="typeRegister">Type</label>
+            <input type="text" id="typeRegister" name="typeRegister" required>
+        </div>
+
+        <!-- <button><a href="index.php">Back to Login</a></button> -->
+        <button>Sign up</button>
+    </form>
+    
+    <form method="post" action="index.php">
+        <button type="submit">Go to Login</button>
+    </form>
+</body>
+</html>
